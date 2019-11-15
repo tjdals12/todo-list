058c60e9186e5818701a95be533cb5e1c490f19f
Seongmin <lll2slll@naver.com>
2019-11-15 18:31:25 +0900

    [2019. 11. 15] Clean

--- client/src/store/modules/modal/types.ts
+++
@@ -1,7 +0,0 @@
-import { ActionType } from 'typesafe-actions';
-import { open, close } from './action';
-
-const actions = { open, close };
-export type modalsActionTypes = ActionType<typeof actions>;
-
-export type modalsStateType = Record<string, boolean>;
